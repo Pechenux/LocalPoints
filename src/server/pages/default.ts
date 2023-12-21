cf8d9836cf950dedfd20960acc64da81f25141f6
Kirill Stupakov <kirill.stupakov@universe-data.ru>
2023-12-21 22:28:21 +0300

    Remove suboptimal code

--- src/server/pages/default.ts
+++
@@ -1,9 +0,0 @@
-import { render } from '../render'
-
-import type { Request, Response } from 'express'
-
-export default async function defaultPage(req: Request, res: Response) {
-  render(req, res)
-
-  res.send()
-}
